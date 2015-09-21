9ed330cdc99d568b1d2b309d9b05038e32f505c7
bogdannechyporenko <bnechyporenko@xebia.com>
2015-09-21 17:55:55 +0200

    added first api

--- test/help_test.js
+++ test/help_test.js
@@ -5,28 +5,27 @@ var cu = require('../lib/commands');
 describe('Simple tests', function () {
     var $this = this;
 
-    $this.timeout(10000);
-    //
-    //it('xl-repo-linker -h', function (done) {
-    //
-    //    return cu.checkCommand('xl-repo-linker -h', function (data) {
-    //        cu.expect(data).contains('Usage: xl-repo-linker1');
-    //    }, done);
-    //});
+    $this.timeout(7000);
+
+    it('xl-repo-linker -h', function (done) {
+
+        return cu.checkCommand('xl-repo-linker -h', function (data) {
+            cu.expect(data).contains('Usage: xl-repo-linker');
+        }, done);
+    });
 
     it('xl-repo-linker --show-size', function (done) {
-        cu.checkCommand('xl-repo-linker --xld-home="/Users/bogdannechyporenko/proj/xl-deploy/package/build/distributions/xl-deploy-5.2.0-SNAPSHOT-server" --show-size', function (data) {
+        cu.checkCommand('xl-repo-linker --show-size', function (data) {
             cu.expect(data).contains('XLD snapshot size is:');
         }, done);
     });
 
-    //it('xl-repo-linker --mode bla', function (done) {
-    //    cu.checkCommand('xl-repo-linker --mode bla', function (data) {
-    //        expect(data).to.equal('Please check your mode value, valid values are [local, jira, google-drive]\n');
-    //        done();
-    //    });
-    //});
-    //
+    it.only('xl-repo-linker --mode bla', function (done) {
+        cu.checkCommand('xl-repo-linker --mode bla', function (data) {
+            cu.expect(data).contains('Please check your mode value, valid values are [local, jira, google-drive]\n');
+        }, done);
+    });
+
     //it('xl-repo-linker --xld-home=IncorrectPath', function (done) {
     //    cu.runAndCheckCurl('xl-repo-linker --xld-home=IncorrectPath', 4, function (data) {
     //        expect(data.configValidation).to.equal('XL Deploy home doesn\'t exist [IncorrectPath]');
