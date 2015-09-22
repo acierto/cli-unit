73f52a977b4788dec14f71dd6a60361a463b420a
bogdannechyporenko <bnechyporenko@xebia.com>
2015-09-22 08:19:29 +0200

    added extra functionality

--- test/help_test.js
+++ test/help_test.js
@@ -5,7 +5,7 @@ var cu = require('../lib/commands');
 describe('Simple tests', function () {
     var $this = this;
 
-    $this.timeout(7000);
+    $this.timeout(8000);
 
     it('xl-repo-linker -h', function (done) {
 
@@ -20,19 +20,18 @@ describe('Simple tests', function () {
         }, done);
     });
 
-    it.only('xl-repo-linker --mode bla', function (done) {
+    it('xl-repo-linker --mode bla', function (done) {
         cu.checkCommand('xl-repo-linker --mode bla', function (data) {
             cu.expect(data).contains('Please check your mode value, valid values are [local, jira, google-drive]\n');
         }, done);
     });
 
-    //it('xl-repo-linker --xld-home=IncorrectPath', function (done) {
-    //    cu.runAndCheckCurl('xl-repo-linker --xld-home=IncorrectPath', 4, function (data) {
-    //        expect(data.configValidation).to.equal('XL Deploy home doesn\'t exist [IncorrectPath]');
-    //        expect(data.fields.length).to.equal(1);
-    //        assert.deepEqual(['xld.home'], data.fields);
-    //        done();
-    //    });
-    //});
+    it('xl-repo-linker --xld-home=IncorrectPath', function (done) {
+        cu.runAndCheckCurl('xl-repo-linker --xld-home=IncorrectPath', 'curl http://0.0.0.0:3000/xlrl/checkConfig', function (data) {
+            cu.expect(data.configValidation).equals('XL Deploy home doesn\'t exist [IncorrectPath]');
+            cu.expect(data.fields.length).equals(1);
+            cu.expect(data.fields).deepEquals(['xld.home']);
+        }, done);
+    });
 
 });
